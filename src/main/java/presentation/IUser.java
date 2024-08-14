a80cbf87cc6395eef74bb50e48e20ba5262b5296
IB <holt.irina@gmail.com>
2024-08-14 21:48:01 +0200

    presentation 1st commit

---
+++ src/main/java/presentation/IUser.java
@@ -0,0 +1,9 @@
+package presentation;
+
+public interface IUser {
+    public void greetUser();
+    public Button choose();
+    public String askLogin();
+    public String askPassword();
+    }
+
